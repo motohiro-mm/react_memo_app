da947c21a98beedaf2c26df63e8b9596c204392a
motohiro-mm <polarbear.dx@gmail.com>
2024-04-01 14:45:44 +0900

    余計なタグを削除

--- my-memo-app/src/MemoList.js
+++ my-memo-app/src/MemoList.js
@@ -7,25 +7,23 @@ export default function MemoList({ memos, selectedMemo, onSelect }) {
   }
 
   return (
-    <section className="memos">
-      <ul>
-        {memos.map((memo) => (
-          <li key={memo.id}>
-            <button
-              className={
-                selectedMemo && selectedMemo.id === memo.id
-                  ? "activeButton"
-                  : "normalButton"
-              }
-              onClick={() => {
-                onSelect(memo);
-              }}
-            >
-              {pickUpMemoTitle(memo.text)}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </section>
+    <ul>
+      {memos.map((memo) => (
+        <li key={memo.id}>
+          <button
+            className={
+              selectedMemo && selectedMemo.id === memo.id
+                ? "activeButton"
+                : "normalButton"
+            }
+            onClick={() => {
+              onSelect(memo);
+            }}
+          >
+            {pickUpMemoTitle(memo.text)}
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 }
